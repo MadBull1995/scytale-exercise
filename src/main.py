7ba35205882310e7f227017c53d4d6b5febd9662
Amit Shmulevitch <amitsh1995@gmail.com>
2024-03-07 21:54:04 +0200

    uncommented ingest phase

--- src/main.py
+++ src/main.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     org_name = "Scytale-exercise"
     api = GitHubAPI()
     process = GithubIntegrationData(api)
-    # data = process.ingest(owner=org_name)
+    data = process.ingest(owner=org_name)
     data = process.transform(owner=org_name)
     process.load(data, owner=org_name)
